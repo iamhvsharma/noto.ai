6e51ee94de164badee4c77ba84eb162b4c4a6310
iamhvsharma <itshvsharma@gmail.com>
2025-07-04 19:09:36 +0530

    Add Signup component with AuthForm for user registration

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,19 @@
+import AuthForm from "@/components/Auth/AuthForm";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div className="mt-20 flex flex-1 flex-col items-center">
+      <Card className="w-full max-w-md">
+        <CardHeader className="mb-4">
+          <CardTitle className="text-center text-3xl">Signup</CardTitle>
+        </CardHeader>
+
+        <AuthForm type="signup" />
+      </Card>
+    </div>
+  );
+};
+
+export default Signup;
